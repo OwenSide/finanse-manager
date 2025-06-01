e6fcc0e596d6171ac59ddb1fe08eb8595c20dcac
Nazar <nazar.honcharuk1@gmail.com>
2025-06-01 16:37:58 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Menedżer finansów',
+        short_name: 'FinManager',
+        description: 'Progresywna aplikacja do zarządzania finansami',
+        theme_color: '#3b82f6',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  base: "/finanse-manager",
+})
